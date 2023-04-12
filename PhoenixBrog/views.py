b529c0c0cd91e02fa92b5501db0b3fe6cd92c9a7
tomotaka-osanai <tomotaka-osanai.524@outlook.jp>
2023-04-13 05:16:01 +0900

    docker コンテナ構成変更  - postgres バージョン変更  - pgAdmin 追加 UI追加修正

--- PhoenixBrog/views.py
+++ PhoenixBrog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-
+from .models import Article
 
 def index(request):
-    context = {'':''}
+    latest_brog_article = Article.objects.order_by('created_at')[0]
+    context = {'article':latest_brog_article}
     return render(request, 'PhoenixBrog/index.html', context)
