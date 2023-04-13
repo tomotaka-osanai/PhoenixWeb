9736cff3c1e1d6b8f09a5d5791afa18b13ec6937
tomotaka-osanai <tomotaka-osanai.524@outlook.jp>
2023-04-14 02:02:52 +0900

    テーブル構造変更

--- PhoenixBlog/models.py
+++ PhoenixBlog/models.py
@@ -1,18 +1,40 @@
 from django.db import models
+from django.views.generic import ListView
+from django.utils import timezone
+
 class Category(models.Model):
     category_name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.category_name
-class Article(models.Model):
+class Contents(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def get_popular_article(self):
+        # 現在日時より過去の記事の中からランダムに10件を取得
+        return self.objects.filter(updated_at__lte=timezone.now()).order_by('?')[:10]
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.IntegerField()
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+class ContentArticle(models.Model):
+    content = models.IntegerField()
+    article = models.IntegerField()
+    class Meta:
+        unique_together = ('content', 'article')
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
--- PhoenixBlog/views.py
+++ PhoenixBlog/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
-from .models import Article
-from django.views.generic import ListView
-from django.utils import timezone
+from .models import Contents
 from django.core.paginator import Paginator
 
 # トップページ表示クラス
 def index(request):
-    latest_brog_article = Article.objects.order_by('created_at')[0]
-    popular_blog_article = get_queryset()
+    # 新着の投稿
+    latest_brog_article = Contents.objects.order_by('created_at')[0]
+    # 人気の記事
+    popular_blog_article = Contents.get_popular_article(Contents)
     context = {'article':latest_brog_article, 'list':popular_blog_article}
     return render(request, 'PhoenixBlog/index.html', context)
 
 # 一覧ページ表示クラス
 def list(request):
-    article_list = Article.objects.all()
+    # 記事一覧
+    article_list = Contents.objects.all()
+    # ページネーション
     paginator = Paginator(article_list, 10)  # 1ページあたり10件表示する
     page = request.GET.get('page')
     posts = paginator.get_page(page)
     return render(request, 'PhoenixBlog/list.html', {'posts': posts})
-
-# 人気の記事を10記事取得
-def get_queryset():
-    # 現在日時より過去の記事の中からランダムに10件を取得
-    return Article.objects.filter(updated_at__lte=timezone.now()).order_by('?')[:10]
