8005f5c231e85d422c1e8dc40f7b202b2375d9d2
tomotaka-osanai <tomotaka-osanai.524@outlook.jp>
2023-04-13 21:54:46 +0900

    一覧ページ作成

--- PhoenixBlog/views.py
+++ PhoenixBlog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Article
 from django.views.generic import ListView
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 # トップページ表示クラス
 def index(request):
@@ -9,6 +10,14 @@ def index(request):
     popular_blog_article = get_queryset()
     context = {'article':latest_brog_article, 'list':popular_blog_article}
     return render(request, 'PhoenixBlog/index.html', context)
+
+# 一覧ページ表示クラス
+def list(request):
+    article_list = Article.objects.all()
+    paginator = Paginator(article_list, 10)  # 1ページあたり10件表示する
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'PhoenixBlog/list.html', {'posts': posts})
 
 # 人気の記事を10記事取得
 def get_queryset():
