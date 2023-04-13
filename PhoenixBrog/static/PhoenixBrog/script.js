8f4e3e4872734f6e9fb750b6178fe4e4dade2b48
tomotaka-osanai <tomotaka-osanai.524@outlook.jp>
2023-04-13 09:53:56 +0900

    トップページ スワイパー追加

--- PhoenixBrog/static/PhoenixBrog/script.js
+++ PhoenixBrog/static/PhoenixBrog/script.js
@@ -1,18 +1,43 @@
+$(function () {
+  initHunburger();
+  initSwiper();
+});
+
 function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("show");
 }
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector("nav ul");
-const links = document.querySelectorAll("nav ul li");
 
-hamburger.addEventListener("click", () => {
-  // ハンバーガーメニューを開閉する
-  navLinks.classList.toggle("open");
-  // メニュー項目をフェードイン・アウトする
-  links.forEach((link) => {
-    link.classList.toggle("fade");
+function initHunburger() {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector("nav ul");
+  const links = document.querySelectorAll("nav ul li");
+
+  hamburger.addEventListener("click", () => {
+    // ハンバーガーメニューを開閉する
+    navLinks.classList.toggle("open");
+    // メニュー項目をフェードイン・アウトする
+    links.forEach((link) => {
+      link.classList.toggle("fade");
+    });
+    // ハンバーガーアイコンをアニメーションする
+    hamburger.classList.toggle("toggle");
   });
-  // ハンバーガーアイコンをアニメーションする
-  hamburger.classList.toggle("toggle");
-});
+}
+
+function initSwiper() {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
